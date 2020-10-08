c67a16acb88634acc9ed382f36a23975834eb7ef
ronbravo <ronbravo1701@gmail.com>
2020-10-08 10:35:33 -0700

    Additional setup for qiankun and single-spa micro-frontend frameworks.

--- .app/template/src/app/app.component.ts
+++ .app/template/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-app-base-ang8',
+  selector: 'app-{name}',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-base-ang8';
+  title = '{title}';
 }
